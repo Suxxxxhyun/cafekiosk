fe566dc960b6a47b1a32a6f37a58feac4c508a30
suhyun <qkrtngus116@naver.com>
2024-01-08 10:09:43 +0900

    test: Repository Layer Test

---
+++ src/test/java/sample/cafekiosk/spring/repository/ProductRepositoryTest.java
@@ -0,0 +1,71 @@
+package sample.cafekiosk.spring.repository;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+@ActiveProfiles("test") //application.yml에 지정한 프로파일을 test로 지정
+//@SpringBootTest
+@DataJpaTest //Jpa관련된 것만 이용해서 서버를 띄움, SpringBootTest보다 가벼움
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
+    @Test
+    void findAllBySellingStatusIn(){
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus") // products 리스트에서 특정 속성을 추출
+                .containsExactlyInAnyOrder( // 추출한 속성들이 순서에 상관없이 주어진 값과 일치해야함
+                        tuple("001", "아메리카노", SELLING), //tuple - AssertJ에서 제공하는 것으로, 여러 값을 하나의 튜플로 묶어줌
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+}
